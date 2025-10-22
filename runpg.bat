c460eb4be40f229619f1aa23ad210004c3fec6f0
Yiyu Liu <yiyli@microsoft.com>
2025-10-22 16:36:28 +0800

    Add .gitignore and update project files - exclude target directory and IDE files

---
+++ runpg.bat
@@ -0,0 +1,1 @@
+java -cp .\target\demo-1.0-SNAPSHOT.jar com.example.MainPG
---
+++ src/main/java/com/example/MainPGConnStr.java
@@ -0,0 +1,42 @@
+package com.example;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class MainPGConnStr {
+
+    
+
+
+    public static void main(String[] args) throws Exception {
+
+        Properties properties = new Properties();
+        try (InputStream input = MainPGConnStr.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find application.properties");
+                return;
+            }
+            // Load the properties file
+            properties.load(input);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        String connString = properties.getProperty("AZURE_PGSQL_CONNECTIONSTRING");
+        
+
+        Connection connection = DriverManager.getConnection(connString);
+        
+
+        System.out.println(connection.toString());
+        
+
+    }
+
+    
+}
